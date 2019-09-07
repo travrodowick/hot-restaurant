9609d2c0292b045a68c8c5bf8b0ba8a202a5ab5d
William Dunn <pikablu112@gmail.com>
2019-09-07 11:25:32 -0700

    added display home page and add new reservation

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
+//Dependancies
+//=======================================================================
 var express = require("express");
 var path = require("path");
 
+//Express App
+//=======================================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+//Data Parsing
+//=======================================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Data (JSON)
+//=======================================================================
+var reservations = [
+    {
+        name: "Billy",
+        time: "5:30 PM",
+        date: "November 28th, 2019",
+        phone: "123-456-7891",
+    }
+]
+console.log(reservations);
+
+//Display home page
+//=======================================================================
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+//Display all Reservations
+//=======================================================================
+app.get("/api/tables", function (req, res) {
+    return res.json(reservations);
+});
+
+//JSON Input
+//=======================================================================
+app.post("/api/reserve", function (req, res) {
+    var newReservation = req.body;
+    newReservation.name = newReservation.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newReservation);
+    reservations.push(newReservation);
+    res.json(newReservation);
+});
+
+//Listening
+//=======================================================================
 app.listen(PORT, function () {
     console.log("App listening on PORT" + PORT);
 });
