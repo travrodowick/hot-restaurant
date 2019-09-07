0b765d68692e601263b77e7fba8616b04cae80b2
Kyle McFadden <burntwist@sbcglobal.net>
2019-09-07 11:26:51 -0700

    server and home updates

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express")
+var path = require("path")
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(_dirname, "home.html"))
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(_dirname, "tables.html"))
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(_dirname, "reserve.html"))
+})
